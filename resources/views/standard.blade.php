b225c3b7376868eeeedba5495b953e3a44641ee1
Afnan <afnam78@gmail.com>
2024-02-16 14:10:52 +0100

    Added style effects

--- resources/views/standard.blade.php
+++ resources/views/standard.blade.php
@@ -4,10 +4,10 @@
             <div class="row">
                 <div class="col-10">
                     <h1 class="mb-0 mt-2">{{ $this->survey->name }}</h1>
-                    @if($this->entry ?? false)
-                    <h3 class="mt-2 text-muted"> {{ $this->entry->surveyed->name ?? '' }} - {{
-                        $this->entry->surveyed->contact_person ?? '' }}
-                    </h3>
+                    @if ($this->entry ?? false)
+                        <h3 class="mt-2 text-muted"> {{ $this->entry->surveyed->name ?? '' }} -
+                            {{ $this->entry->surveyed->contact_person ?? '' }}
+                        </h3>
                     @endif
                 </div>
                 <div class="col-2 text-right">
@@ -16,40 +16,51 @@
             </div>
         </div>
         @php($numberQuestion = 1)
-        @foreach($this->survey->sections as $index => $section)
-        @include('survey::sections.single')
-        @php($numberQuestion += $section->questions->count())
+        @foreach ($this->survey->sections as $index => $section)
+            @include('survey::sections.single')
+            @php($numberQuestion += $section->questions->count())
         @endforeach
     </div>
-    @if($this->survey->status == MattDaneshvar\Survey\Library\Constants::SURVEY_STATUS_PROCESS && $sendForm)
-    @if (session()->has('answersAlert'))
-    <div id="answersAlert" class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
-        <span> {{ session('answersAlert') }}</span>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    @endif
-    <div class="d-flex flex-row-reverse">
-        @if ($this->entry->lang == 'en')
-        <button id="send-en" class="btn btn-success my-3 mr-2"
-            onclick="confirm('¿Está seguro? Esta acción no puede deshacerse.') || event.stopImmediatePropagation();"
-            wire:click="sendAnswers">
-            Send
-        </button>
-        <button class="btn btn-primary my-3 mr-2" wire:click="saveAnswers">
-            Save
-        </button>
-        @else
-        <button id="send-es" class="btn btn-success my-3"
-            onclick="confirm('¿Está seguro? Esta acción no puede deshacerse.') || event.stopImmediatePropagation();"
-            wire:click="sendAnswers">
-            Enviar
-        </button>
-        <button class="btn btn-primary my-3 mr-2" wire:click="saveAnswers">
-            Guardar
-        </button>
+    @if ($this->survey->status == MattDaneshvar\Survey\Library\Constants::SURVEY_STATUS_PROCESS && $sendForm)
+        @if (session()->has('answersAlert'))
+            <div id="answersAlert" class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
+                <span> {{ session('answersAlert') }}</span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
         @endif
-    </div>
+        <div class="d-flex flex-row-reverse">
+            @if ($this->entry->lang == 'en')
+                <button id="send-en" class="btn btn-success my-3 mr-2"
+                    onclick="confirm('¿Está seguro? Esta acción no puede deshacerse.') || event.stopImmediatePropagation();"
+                    wire:click="sendAnswers">
+                    Send
+                </button>
+                <button class="btn btn-primary my-3 mr-2" wire:click="saveAnswers">
+                    Save
+                </button>
+            @else
+                <button id="send-es" class="btn btn-success my-3"
+                    onclick="confirm('¿Está seguro? Esta acción no puede deshacerse.') || event.stopImmediatePropagation();"
+                    wire:click="sendAnswers">
+                    Enviar
+                </button>
+                <button class="btn btn-primary my-3 mr-2" wire:click="saveAnswers">
+                    Guardar
+                </button>
+            @endif
+        </div>
     @endif
+    <style>
+        .collapsed .tab-arrow {
+            transform: rotate(180deg);
+            transition: transform 0.3s;
+        }
+
+        .tab-arrow {
+            transition: transform 0.3s;
+
+        }
+    </style>
 </div>
