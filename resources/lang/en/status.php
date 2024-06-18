c4f84d5b03f28d576dee06722659a9452d8817c7
GabrielBarber <gbarber@lanzaderaproyectos.com>
2024-06-18 14:34:49 +0200

    change status name

--- resources/lang/en/status.php
+++ resources/lang/en/status.php
@@ -6,7 +6,7 @@ return [
     'survey' => [
         Constants::SURVEY_STATUS_DRAFT => 'Borrador',
         Constants::SURVEY_STATUS_PROCESS => 'En proceso',
-        Constants::SURVEY_STATUS_COMPLETED => 'Terminada',
+        Constants::SURVEY_STATUS_COMPLETED => 'Validado',
         Constants::SURVEY_STATUS_SEND_ERROR => 'Error',
         Constants::SURVEY_STATUS_CLOSED => 'Cerrada',
     ],
