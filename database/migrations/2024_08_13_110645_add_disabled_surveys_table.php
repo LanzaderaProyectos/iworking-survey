c85aa1186ed7fe02048dd133b997a3254235857d
GabrielBarber <gbarber@lanzaderaproyectos.com>
2024-08-13 13:12:40 +0200

    add migrations

---
+++ database/migrations/2024_08_13_110645_add_disabled_surveys_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('surveys', 'disabled_at')) {
+            Schema::table('surveys', function (Blueprint $table) {
+                $table->timestamp('disabled_at')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasColumn('surveys', 'disabled_at')) {
+            Schema::table('surveys', function (Blueprint $table) {
+                $table->dropColumn('disabled_at');
+            });
+        }
+    }
+};
